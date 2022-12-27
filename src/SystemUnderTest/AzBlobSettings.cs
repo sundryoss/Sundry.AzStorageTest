f2e03aec2aeaf2385fa5fa868d14b9e1c72ec2cf
anktsrkr <ankt.srkr@gmail.com>
2022-12-27 13:55:58 +0000

    Add project files.

---
+++ src/SystemUnderTest/AzBlobSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemUnderTest;
+
+public sealed class AzBlobSettings
+{
+    public required string ConnectionString { get; set; }
+
+}
---
+++ src/SystemUnderTest/Program.cs
@@ -0,0 +1,50 @@
+﻿using Azure;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using System.Reflection.Metadata;
+using SystemUnderTest;
+using static System.Reflection.Metadata.BlobBuilder;
+
+
+using IHost host = Host
+    .CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((context, config) =>
+    {
+        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+              .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true);
+    })
+.Build();
+
+
+
+await UploadFileToAzBlob(host.Services);
+await host.RunAsync();
+
+
+static async Task UploadFileToAzBlob(IServiceProvider services)
+{
+    var config = services.GetRequiredService<IConfiguration>();
+    var azBlobConnectionString = config.GetValue<string>("AzBlobSettings:ConnectionString");
+    var azBlobContainerName = config.GetValue<string>("AzBlobSettings:ContainerName");
+    var fileName = config.GetValue<string>("AzBlobSettings:FileName")!;
+
+    using FileStream stream = new(fileName, FileMode.Open);
+    
+    BlobContainerClient container = new (azBlobConnectionString, azBlobContainerName);
+
+    await container.CreateIfNotExistsAsync();
+
+    try
+    {
+        var client = container.GetBlobClient(fileName);
+        await client.UploadAsync(stream);
+    }
+    catch (Exception)
+    {
+        throw;
+    }
+}
