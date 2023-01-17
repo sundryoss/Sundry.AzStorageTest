a9a0cf6f13730ec7095d897a17eadb65bdd9684d
Ankit Sarkar <ankit.sarkar@gmail.com>
2023-01-17 08:29:10 +0000

    Updated Testcases with Sequence

--- tests/SystemUnderTest.IntegrationTest/IntegrationTests.cs
+++ tests/SystemUnderTest.IntegrationTest/IntegrationTests.cs
@@ -1,39 +1,34 @@
 ﻿using Azure;
+using Azure.Storage.Blobs;
 using SystemUnderTest.Interface;
 
-namespace SystemUnderTest.IntegrationTest
+namespace SystemUnderTest.IntegrationTest;
+
+[Collection(nameof(AzuriteContainer))]
+[TestCaseOrderer("SystemUnderTest.IntegrationTest.SequenceOrderer", "SystemUnderTest.IntegrationTest")]
+
+public class IntegrationTests
 {
-    [Collection(nameof(AzuriteContainer))]
-    public class IntegrationTests
+    private readonly StringWriter Output = new();
+    private readonly IAzBlobService _azBlobService;
+
+    public IntegrationTests(IAzBlobService azBlobService, AzuriteContainer azuriteInstance)
     {
-        private readonly StringWriter Output = new();
-        private readonly IAzBlobService _azBlobService;
+        Console.SetOut(Output);
+       _azBlobService = azBlobService;
+    }
 
-        public IntegrationTests(IAzBlobService azBlobService, AzuriteContainer azuriteInstance)
-        {
-            Console.SetOut(Output);
-           _azBlobService = azBlobService;
-        }
+    [Fact, TestSequence(0)]
+    public async Task File_Upload_Success()
+    {
+        await Program.UploadFileToAzBlobAsync(_azBlobService);
+        Assert.Contains("File uploaded successfully", Output.ToString());
+    }
 
-        [Fact]
-        public async Task File_Upload_Success()
-        {
-            await Program.UploadFileToAzBlobAsync(_azBlobService);
-            Assert.Contains("File uploaded successfully", Output.ToString());
-        }
-
-        [Fact]
-        public async Task File_Download_Success()
-        {
-            await Program.UploadFileToAzBlobAsync(_azBlobService);
-            await Program.DownloadBlobAsync(_azBlobService);
-            Assert.Contains("Downloaded data: This is sample file.", Output.ToString());
-        }
-
-        [Fact]
-        public async Task File_Download_Failure()
-        {
-          await  Assert.ThrowsAsync<RequestFailedException>(() => Program.DownloadBlobAsync(_azBlobService));
-        }
+    [Fact, TestSequence(1)]
+    public async Task File_Download_Success()
+    {
+        await Program.DownloadBlobAsync(_azBlobService);
+        Assert.Contains("Downloaded data: This is sample file.", Output.ToString());
     }
 }
---
+++ tests/SystemUnderTest.IntegrationTest/TestSequenceAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace SystemUnderTest.IntegrationTest;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+public class TestSequenceAttribute : Attribute
+{
+    public TestSequenceAttribute(int priority)
+    {
+        Priority = priority;
+    }
+
+    public int Priority { get; private set; }
+}
